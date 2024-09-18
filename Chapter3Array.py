0c4f1444ac43c4d5c6d6b3b08678f206d51345af
OzzieTheGreat <90358793+OzzieTheGreat@users.noreply.github.com>
2024-09-18 09:58:52 -0400

    Add files via upload

---
+++ Chapter3Array.py
@@ -0,0 +1,14 @@
+def insertElement(arr, n, x, pos):
+    for i in range(n - 1, pos - 1, -1):
+        arr[i + 1] = arr[i]
+    arr[pos] = x
+if __name__ == '__main__':
+    arr = [3, 6, 4, 8, -1]
+    n = 4
+    x = 7
+    pos = 0
+    insertElement(arr, n, x, pos)
+    n += 1
+    print("Insertion of element : ")
+    for i in range(0, n):
+        print(arr[i], end=' ')
